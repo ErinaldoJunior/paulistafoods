2b7461d01c7efb6ac8c42889a32581468e1ee555
Erinaldo Junior <erinaldogomesjr@gmail.com>
2022-12-27 23:21:37 +0000

    att

--- script/script.js
+++ script/script.js
@@ -39,7 +39,7 @@ function animacaoScroll() {
   const sections = document.querySelectorAll(".js-scroll");
 
   if (sections.length) {
-    const windowMetade = window.innerHeight * 0.8;
+    const windowMetade = window.innerHeight * 0.9;
 
     function animaScroll() {
       sections.forEach((section) => {
